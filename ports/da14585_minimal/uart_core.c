edf4b2acb1e3157d67ce9db232f8d4f6a9b4a206
Maximilian Wassmann Gregerse <max@otiom.com>
2020-05-06 13:53:43 +0200

    removed old commented code

--- ports/da14585_minimal/uart_core.c
+++ ports/da14585_minimal/uart_core.c
@@ -14,37 +14,11 @@ int mp_hal_stdin_rx_chr(void) {
     c = uart_read_byte(UART1);
 
     return c;
-
-    // unsigned char c = 0;
-    // #if MICROPY_MIN_USE_STDOUT
-    // int r = read(STDIN_FILENO, &c, 1);
-    // (void)r;
-    // #elif MICROPY_MIN_USE_STM32_MCU
-    // // wait for RXNE
-    // while ((USART1->SR & (1 << 5)) == 0) {
-    // }
-    // c = USART1->DR;
-    // #endif
-    // return c;
 }
 
 // Send string of given length -- replace this?!
 void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
-
     while (len--) {
         uart_write_byte(UART1, *str++);
     }
-
-    // printf_string(UART1, *str_ptr); //-Werror=discarded-qualifiers TODO
-    // #if MICROPY_MIN_USE_STDOUT
-    // int r = write(STDOUT_FILENO, str, len);
-    // (void)r;
-    // #elif MICROPY_MIN_USE_STM32_MCU
-    // while (len--) {
-    //     // wait for TXE
-    //     while ((USART1->SR & (1 << 7)) == 0) {
-    //     }
-    //     USART1->DR = *str++;
-    // }
-    // #endif
 }
